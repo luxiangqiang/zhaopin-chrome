95c1d7827bffcf0c14ea022a57bbd0064c0ea43b
luxiangqiang <1040500646@qq.com>
2022-08-23 18:45:32 +0800

    feat: 完成应届生/实习生自动发布职位

---
+++ src/modules/guopin/types.ts
@@ -0,0 +1,14 @@
+export interface IFormat {
+  title: string;
+  code: string;
+  type: string;
+  salaryFrom: string;
+  salaryTo: string;
+  amount: number;
+  salmonths: number;
+  description: string;
+  category: string[];
+  city: string[];
+  education: string;
+  specialized: string[]
+}
